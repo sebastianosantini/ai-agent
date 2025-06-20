d7916f858b8a4988a81b10cc0eabdfb94c419507
Sebastiano Santini <sebsantini96@gmail.com>
2025-06-20 07:12:44 +0200

    Added verbose feature

--- main.py
+++ main.py
@@ -1,23 +1,31 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 import sys
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
+def main():
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
 
-client = genai.Client(api_key=api_key)
+    args = sys.argv[1:]
+    if not args:
+        print("no args provided...")
+        sys.exit(1)
+    
+    user_prompt = " ".join(args)
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+    response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
 
-if len(sys.argv) < 2:
-    print("no content provided...")
-    exit(1)
-
-if "main.py" in sys.argv:
-    sys.argv.remove("main.py")
-
-content = " ".join(sys.argv)
-
-response = client.models.generate_content(model="gemini-2.0-flash-001", contents=content)
-
-print(response.text)
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}\nResponse tokens: {response.usage_metadata.candidates_token_count}\n")
+    if "--verbose" in args:
+        print(f"User prompt: {user_prompt}")
+        print(f"Response: {response.text}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    else:
+        print(f"Response: {response.text}")
+if __name__ == "__main__":
+    main()
