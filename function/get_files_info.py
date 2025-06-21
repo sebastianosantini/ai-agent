ca9422883c27a9fc75f51b4fa0fd91bc5e32563e
Sebastiano Santini <sebsantini96@gmail.com>
2025-06-21 10:49:28 +0200

    Added write_file.py and tested its functionality

--- function/get_files_info.py
+++ function/get_files_info.py
@@ -1,10 +1,14 @@
 import os
 
+
 def get_files_info(working_directory, directory=None):
     abs_working_directory = os.path.abspath(working_directory)
     target_dir = ""
     if directory:
-        target_dir = os.path.abspath(os.path.join(abs_working_directory, directory))
+        target_dir = os.path.abspath(
+            os.path.join(
+                abs_working_directory, directory
+                ))
     if target_dir and not target_dir.startswith(abs_working_directory):
         return f'Error: Cannot list "{target_dir}" as it is outside the permitted working directory'
     if not os.path.isdir(target_dir):
@@ -13,16 +17,22 @@ def get_files_info(working_directory, directory=None):
     try:
         dir_content = os.listdir(target_dir)
         result = []
-        for items in dir_content:
-            result.append(f"- {items}: file_size={os.path.getsize(os.path.join(target_dir, items))} bytes, is_dir={os.path.isdir(os.path.join(target_dir, items))}")
+        for item in dir_content:
+            item_size = os.path.getsize(os.path.join(target_dir, item))
+            is_item_dir = os.path.isdir(os.path.join(target_dir, item))
+            result.append(f"- {item}: file_size={item_size} bytes, is_dir={is_item_dir}")
 
         return "\n".join(result)
     except Exception as e:
         return f"Error: {e}"
 
+
 def get_file_content(working_directory, file_path):
     abs_working_directory = os.path.abspath(working_directory)
-    target_file_path = os.path.abspath(os.path.join(abs_working_directory, file_path))
+    target_file_path = os.path.abspath(
+        os.path.join(
+            abs_working_directory, file_path
+            ))
 
     if target_file_path and not target_file_path.startswith(abs_working_directory):
         return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
@@ -44,4 +54,27 @@ def get_file_content(working_directory, file_path):
     except Exception as e:
         return f"Error: {e}"
 
-        
+
+def write_file(working_directory, file_path, content):
+    abs_working_directory = os.path.abspath(working_directory)
+    target_file_path = os.path.abspath(
+        os.path.join(
+            abs_working_directory, file_path
+            ))
+
+    if target_file_path and not target_file_path.startswith(abs_working_directory):
+        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+    
+    try:
+        if not os.path.exists(target_file_path):
+            file_directory = os.path.dirname(target_file_path)
+
+            if not os.path.isdir(file_directory):
+                os.makedirs(file_directory)
+
+            with open(target_file_path, "w") as f:
+                f.write(content)
+
+        return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
+    except Exception as e:
+        return f"Error: {e}"
--- tests.py
+++ tests.py
@@ -1,12 +1,12 @@
-from function.get_files_info import get_files_info, get_file_content
+from function.get_files_info import write_file
 
 def tests():
-    result = get_file_content("calculator", "main.py")
+    result = write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")
     print(result)
-    result = get_file_content("calculator", "pkg/calculator.py")
+    result = write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")
     print(result)
-    result = get_file_content("calculator", "/bin/cat")
+    result = write_file("calculator", "/temp/temp.txt", "this should not be allowed")
     print(result)
-
+    
 if __name__ == "__main__":
     tests()
